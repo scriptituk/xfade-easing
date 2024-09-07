b8f64acf73d3cf6e636852b847afcfdeb36499bb
Raymond Luckhurst <info@scriptit.uk>
2024-09-07 22:37:39 +0100

    Add files via upload

---
+++ glsl/SimpleBookCurl.glsl
@@ -0,0 +1,74 @@
+// Name: SimpleBookCurl
+// Author: Raymond Luckhurst
+// License: MIT
+// Adapted from SimplePageCurl
+// see simple page curl effect by Andrew Hung, https://www.shadertoy.com/view/ls3cDB
+// and https://andrewhungblog.wordpress.com/2018/04/29/page-curl-shader-breakdown/
+
+const float M_PI = 3.14159265359;
+const float M_PI_2 = 1.57079632679;
+
+uniform int angle; // = 150
+uniform float radius; // = 0.1
+uniform float shadow; // = 0.2
+
+vec4 transition (vec2 uv) {
+    // setup
+    float ang = mod(mod(float(angle), 360.) + 360., 360.); // 0 <= ang < 360
+    vec2 q = vec2(1., -1.); // quadrant corner
+    if (ang >= 90.) q.y = 1.; // (1., 1.);
+    if (ang >= 180.) q.x = -1.; // (-1., 1.);
+    if (ang >= 270.) q.y = -1.; // (-1., -1.);
+    float phi = radians(ang) - M_PI_2; // target curl angle
+    vec2 dir = normalize(vec2(cos(phi) * ratio, sin(phi))); // direction unit vector
+    vec2 i = abs(dir);
+    float k = (i.x == 0.) ? M_PI_2 : atan(i.y, i.x); // absolute curl angle
+    i = dir * dot(q * .5, dir); // initial position, curl axis on corner
+    float m1 = length(i); // length for rotating
+    float m2 = M_PI * radius; // length of half-cylinder arc
+
+    // get new angle & progress point
+    float rad = radius; // working radius
+    vec2 p; // working curl axis point
+    float m = (m1 + m2) * progress; // current position along lengths
+    if (m < m1) { // rotating page
+        phi = k * (1. + cos(m / m1 * M_PI)) * .5; // eased new absolute curl angle
+        dir = normalize(vec2(cos(phi), sin(phi)) * q * .5); // new direction
+        p = (m1 - m) * dir;
+        // TODO: prevent small radii crossing spine
+    } else { // straightening curl
+        if (m2 > 0.)
+            rad *= pow(1. - (m - m1) / m2, 2.); // eased new radius
+        dir = vec2(q.x, 0.); // new direction
+        p = vec2(0., 0.);
+    }
+
+    // get point relative to curl axis
+    i = uv - .5; // distance of current point from centre
+    float dist = dot(i - p, dir); // distance of point from curl axis
+    p = i - dir * dist; // point perpendicular to curl axis
+
+    // map point to curl
+    vec4 a = getFromColor(uv), b = getToColor(uv), c = b;
+    bool s = false; // shadow flag
+    if (dist < 0.) { // point is over flat A
+        c = a;
+        p = (p + dir * (M_PI * rad - dist)) * vec2(-1., 1.) + .5;
+        if (p.x >= 0. && p.x <= 1. && p.y >= 0. && p.y <= 1.) // on flat back of A
+            c = getToColor(p);
+    } else if (rad > 0.) { // curled A
+        // map to cylinder point
+        phi = asin(dist / rad);
+        vec2 p2 = (p + dir * (M_PI - phi) * rad) * vec2(-1., 1.) + .5;
+        vec2 p1 = p + dir * phi * rad + .5;
+        if (p2.x >= 0. && p2.x <= 1. && p2.y >= 0. && p2.y <= 1.) // on curling back of A
+            c = getToColor(p2), s = true;
+        else if (p1.x >= 0. && p1.x <= 1. && p1.y >= 0. && p1.y <= 1.) // on curling front of A
+            c = getFromColor(p1);
+        else // on B
+            s = true;
+    }
+    if (s) // TODO: ok over A, makes a tideline over B for large radius
+        c.rgb *= pow(clamp(abs(dist - rad) / rad, 0., 1.), shadow);
+    return c;
+}
--- glsl/SimplePageCurl.glsl
+++ glsl/SimplePageCurl.glsl
@@ -1,11 +1,11 @@
 // Name: SimplePageCurl
 // Author: Andrew Hung
 // License: MIT
-// Adapted by Raymond Luckhurst from:
-// https://www.shadertoy.com/view/ls3cDB
-// see https://andrewhungblog.wordpress.com/2018/04/29/page-curl-shader-breakdown/
+// Adapted by Raymond Luckhurst
+// see simple page curl effect by Andrew Hung, https://www.shadertoy.com/view/ls3cDB
+// and https://andrewhungblog.wordpress.com/2018/04/29/page-curl-shader-breakdown/
 
-const float M_PI = 3.14159265358;
+const float M_PI = 3.14159265359;
 
 uniform int angle; // = 80
 uniform float radius; // = 0.1
@@ -16,33 +16,43 @@ uniform float shadow; // = 0.2
 
 vec4 transition (vec2 uv) {
     // setup
-    float phi = M_PI * float(angle) / 180. - M_PI / 2.; // target curl angle
+    float ang = mod(mod(float(angle), 360.) + 360., 360.); // 0 <= ang < 360
+    vec2 q = vec2(1., -1.); // quadrant corner
+    if (ang >= 90.) q.y = 1.; // (1., 1.);
+    if (ang >= 180.) q.x = -1.; // (-1., 1.);
+    if (ang >= 270.) q.y = -1.; // (-1., -1.);
+    float phi = radians(ang) - M_PI / 2.; // target curl angle
     vec2 dir = normalize(vec2(cos(phi) * ratio, sin(phi))); // direction unit vector
-    vec2 i = dir * dot(sign(dir) * .5, dir); // initial position, curl axis on quadrant corner
-    vec2 f = -(i + dir * radius); // final position, curl just out of view
-    vec2 m = f - i; // travel extent, perpendicular to curl axis
+    vec2 i = dir * dot(q * .5, dir); // initial position, curl axis on corner
+    vec2 f = -(i + dir * radius * 2); // final position, curl & shadow just out of view
+    vec2 m = f - i; // path extent, perpendicular to curl axis
 
     // get point relative to curl axis
-    vec2 p = i + m * (uncurl ? 1. - progress : progress); // current position, origin
-    vec2 q = uv - .5; // distance of current point from centre
+    vec2 p = i + m * (uncurl ? 1. - progress : progress); // current axis point from origin
+    q = uv - .5; // distance of current point from centre
     float dist = dot(q - p, dir); // distance of point from curl axis
     p = q - dir * dist; // point perpendicular to curl axis
 
     // map point to curl
     vec4 a = getFromColor(uv), b = getToColor(uv), c = uncurl ? a : b;
-    bool o = false, s = false; // opacity & shadow flags
-    if (dist < 0.) { // point is over flat A
+    bool r = false, o = false, s = false; // roll & opacity & shadow flags
+    if (dist < 0.) { // point is over flat or rolling A
         c = uncurl ? b : a;
         if (!roll) {
             p += dir * (M_PI * radius - dist) + .5;
-            if (p.x >= 0. && p.x <= 1. && p.y >= 0. && p.y <= 1.) // on flat back of A
-                c = uncurl ? getToColor(p) : getFromColor(p), o = true;
+            r = true;
+        } else if (-dist < radius) {
+            phi = asin(-dist / radius);
+            p += dir * (M_PI + phi) * radius + .5;
+            r = true;
         }
+        if (r && p.x >= 0. && p.x <= 1. && p.y >= 0. && p.y <= 1.) // on back of A
+            c = uncurl ? getToColor(p) : getFromColor(p), o = true;
     } else if (radius > 0.) { // point is over curling A or flat B
         // map to cylinder point
-        float theta = asin(dist / radius);
-        vec2 p2 = p + dir * (M_PI - theta) * radius + .5;
-        vec2 p1 = p + dir * theta * radius + .5;
+        phi = asin(dist / radius);
+        vec2 p2 = p + dir * (M_PI - phi) * radius + .5;
+        vec2 p1 = p + dir * phi * radius + .5;
         if (p2.x >= 0. && p2.x <= 1. && p2.y >= 0. && p2.y <= 1.) // on curling back of A
             c = uncurl ? getToColor(p2) : getFromColor(p2), o = s = true;
         else if (p1.x >= 0. && p1.x <= 1. && p1.y >= 0. && p1.y <= 1.) // on curling front of A
